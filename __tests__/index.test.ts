e9a63d44565ae4f3f570117e50a0341b0d3f48c2
ippachi <ippachi1018@gmail.com>
2023-08-02 21:18:53 +0900

    Single line erb content

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -12,3 +12,20 @@ test("format html", async () => {
 `
   )
 })
+
+test("format signle line <% %>", async () => {
+  const code =
+`<DIV>
+  test
+  <% test %>
+</DIV>
+`
+  const result = await prettier.format(code, { parser: "eruby-parse", plugins: [erubyParse] })
+  expect(result).toBe(
+`<div>
+  test
+  <% test %>
+</div>
+`
+  )
+})
--- index.ts
+++ index.ts
@@ -8,35 +8,53 @@ const languages = [
   }
 ]
 
-async function parse(text: string, options: ParserOptions): Promise<AST> {
-  return await prettier.format(text, { parser: "html" })
-}
+class ErbPrettierPlugin {
+  public astFormat: string
+  private matchResult: RegExpMatchArray | null
 
-function locStart(node: object): number {
-  return 0;
-}
+  constructor() {
+    this.matchResult = null
+    this.astFormat = "eruby-ast"
+  }
 
-function locEnd(node: object): number {
-  return 0;
-}
+  async parse(text: string, options: ParserOptions): Promise<AST> {
+    this.matchResult = text.match(/<%\s.*\s%>/)
+    const targetText = this.replaceErbElementToMarker(text, this.matchResult)
+    return await prettier.format(targetText, { parser: "html" })
+  }
 
-const parsers = {
-  "eruby-parse": {
-    parse,
-    astFormat: "eruby-ast",
-    locStart,
-    locEnd
+  locStart(node: object): number {
+    return 0;
+  }
+
+  locEnd(node: object): number {
+    return 0;
+  }
+
+  print(path: AstPath, options: object, print: (selector: AstPath<any>) => Doc): Doc {
+    let result = path.getNode()
+    result = result.replace(/<erb \/>/, this.matchResult?.[0])
+    return result;
+  }
+
+  private replaceErbElementToMarker(originalText: string, matchResult: RegExpMatchArray | null): string {
+    if (matchResult?.index !== undefined) {
+      return originalText.substring(0, matchResult.index) +
+        '<erb />' +
+        originalText.substring(matchResult.index + matchResult[0].length)
+    }
+    return originalText
   }
 }
 
-function print(path: AstPath, options: object, print: (selector: AstPath<any>) => Doc): Doc {
-  return path.getNode();
+const erbPrettierPlugin = new ErbPrettierPlugin()
+
+const parsers = {
+  "eruby-parse": erbPrettierPlugin
 }
 
 const printers = {
-  "eruby-ast": {
-    print,
-  }
+  "eruby-ast": erbPrettierPlugin
 }
 
 const options = {}
