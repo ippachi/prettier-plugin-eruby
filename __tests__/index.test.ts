ee2bb06cf3e0fc455467c707c25c859ea54942df
ippachi <ippachi1018@gmail.com>
2023-08-13 10:37:01 +0900

    Add <%= %> test

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -41,3 +41,43 @@ test("format invalid indent multi line <% %>", async () => {
 `
   )
 })
+
+test("format invalid indent single line <%= %>", async () => {
+  const code =
+`<DIV>
+  test
+      <%= test %>
+</DIV>
+`
+  const result = await prettier.format(code, { parser: "eruby-parse", plugins: [erubyParse] })
+  expect(result).toBe(
+`<div>
+  test
+  <%= test %>
+</div>
+`
+  )
+})
+
+test("format invalid indent multi line <%= %>", async () => {
+  const code =
+`<DIV>
+  test
+      <%=
+        test1
+        test2
+      %>
+</DIV>
+`
+  const result = await prettier.format(code, { parser: "eruby-parse", plugins: [erubyParse] })
+  expect(result).toBe(
+`<div>
+  test
+  <%=
+    test1
+    test2
+  %>
+</div>
+`
+  )
+})
