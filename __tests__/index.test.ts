82256c3a9f1ef834c924b67e991479c6443445a0
ippachi <ippachi1018@gmail.com>
2023-08-13 10:26:21 +0900

    Remove unneed test

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -2,55 +2,6 @@ import {expect, test} from '@jest/globals';
 import * as prettier from "prettier"
 import erubyParse from "../index"
 
-test("format html", async () => {
-  const code =
-`<DIV>test</DIV>
-`
-  const result = await prettier.format(code, { parser: "eruby-parse", plugins: [erubyParse] })
-  expect(result).toBe(
-`<div>test</div>
-`
-  )
-})
-
-test("format signle line <% %>", async () => {
-  const code =
-`<DIV>
-  test
-  <% test %>
-</DIV>
-`
-  const result = await prettier.format(code, { parser: "eruby-parse", plugins: [erubyParse] })
-  expect(result).toBe(
-`<div>
-  test
-  <% test %>
-</div>
-`
-  )
-})
-
-test("format multi line <% %>", async () => {
-  const code =
-`<DIV>
-  test
-  <%
-    test
-  %>
-</DIV>
-`
-  const result = await prettier.format(code, { parser: "eruby-parse", plugins: [erubyParse] })
-  expect(result).toBe(
-`<div>
-  test
-  <%
-    test
-  %>
-</div>
-`
-  )
-})
-
 test("format invalid indent single line <% %>", async () => {
   const code =
 `<DIV>
